57ffeafc6851d90b81147018bed894ae3fa85ca3
Aschen (Adrien Maret) <amaret93@gmail.com>
2018-11-14 17:28:01 +0100

    add generate filter route

--- generate-geofence.js
+++ generate-geofence.js
@@ -2,7 +2,7 @@ const
   { Kuzzle } = require('kuzzle-sdk'),
   georandom = require('geojson-random')
 
-const kuzzle = new Kuzzle('websocket', { host: 'localhost', port: 7512 });
+const kuzzle = new Kuzzle('websocket', { host: '51.15.139.200', port: 7512 });
 
 const generatePolygon = () => georandom.polygon(1).features[0].geometry.coordinates[0].map(c => [c[1], c[0]])
 
@@ -11,7 +11,7 @@ const run = async () => {
     await kuzzle.connect();
 
     const promises = []
-    for (let i = 0; i < 1000; ++i) {
+    for (let i = 0; i < 2000; ++i) {
 
       const points = generatePolygon();
       const document = {
