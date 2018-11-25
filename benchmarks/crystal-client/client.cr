6c9b29e763a9b1b1810a01ee17ca9e89c29e9368
Aschen (Adrien Maret) <amaret93@gmail.com>
2018-11-25 23:05:45 +0100

    fix parse document from redis

---
+++ benchmarks/crystal-client/client.cr
@@ -0,0 +1,36 @@
+require "benchmark"
+require "http/web_socket"
+require "json"
+
+message = JSON.build do |json|
+  json.object do
+    json.field "controller", "geofencing-marketing/geofence"
+    json.field "action", "test"
+    json.field "lat", ARGV[0]
+    json.field "lng", ARGV[1]
+  end
+end
+
+client = HTTP::WebSocket.new(URI.parse("ws://localhost:7512"))
+
+channel = Channel(String).new
+
+spawn do
+  count = 100
+
+    while count > 0
+      time = Benchmark.realtime do
+        client.send(message)
+        count -= 1
+        channel.receive
+      end
+    puts time
+  end
+end
+
+
+client.on_message do |msg|
+  channel.send("")
+end
+
+client.run()
