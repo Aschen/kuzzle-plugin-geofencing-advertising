0b39d27665a2e206f0ce3b7e6f05883e68ef4248
Aschen (Adrien Maret) <amaret93@gmail.com>
2018-11-26 11:29:56 +0100

    fix gatling docker

--- benchmarks/gatling/docker.sh
+++ benchmarks/gatling/docker.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+echo $JAVA_OPTS
+
 docker run -it --rm \
   -v "$(pwd)/benchmarks/gatling/user-files/:/opt/gatling/user-files" \
   -v "$(pwd)/benchmarks/gatling/results/:/opt/gatling/results" \
+  -e JAVA_OPTS="$JAVA_OPTS" \
   denvazh/gatling
